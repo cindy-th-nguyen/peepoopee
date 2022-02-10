a0616d8c71649ab602a829d3bf4f884b279aed3b
Cindy Nguyen <cindynguyn@gmail.com>
2022-02-10 22:26:21 +0100

    Create ios project

---
+++ Peepoopee/Peepoopee WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Peepoopee WatchKit Extension
+//
+//  Created by Cindy Nguyen on 10/02/2022.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
